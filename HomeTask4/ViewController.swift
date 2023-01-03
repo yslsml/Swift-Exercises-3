59fc8013b3401677fbcc1541e786815536395ac2
Milana Antonova <mmf.antonovam@gmail.com>
2023-01-03 14:06:36 +0300

    done task

--- HomeTask4/ViewController.swift
+++ HomeTask4/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tupleArray = [(1, "x"), (4, "y"), (6, "a")]
+        print(task(tupleArray))
     }
 
+    func task(_ tupleArray: [(Int, String)]) -> [(Int, String)] {
+        return tupleArray
+            .map({ ($0.0 * $0.0, $0.1) })
+            .filter({ $0.0 % 2 == 0 })
+            .sorted(by: { $0.1 < $1.1 })
+    }
 
 }
 
